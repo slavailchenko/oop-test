f7cac7a24b9255011aab752e901fde6f0b35e343
slavailchenko35 <slavailchenko35@gmail.com>
2018-10-12 13:16:54 +0300

    v1.4

--- promise.js
+++ promise.js
@@ -1,88 +1,116 @@
- function A () {
-	let _a, _b, _c;
-	
-	let _setA = () => { 
-			_a = Math.floor(Math.random() * (10000 - 10 + 1) + 10)
-			return _a;
+
+function A () {
+ 
+	_setA = () => { 
+		return new Promise(resolve => {
+		   const time_A = Math.floor(Math.random() * (130 - 50 + 1) + 50);
+		    setTimeout(() => {
+  		       this._a = Math.floor(Math.random() * (10000 - 10 + 1) + 10);
+			// console.log (this._a);
+			resolve (time_A); 
+			// console.log(time_A);
+			}, time_A);
+		});
 	};
 
-	let setTimeout_A = () => {
-		let time_A = Math.floor(Math.random() * (130 - 50 + 1) + 50);
-		setTimeout(_setA(), time_A);
-		return time_A;
+	_setB = () => {
+		
+		return new Promise(resolve => {
+		   const time_B = Math.floor(Math.random() * (220 - 30 + 1) + 30);
+		    setTimeout(() => {
+  		        this._b = new Date().getTime() / 3;
+			// console.log (this._b);
+			resolve (time_B);
+			// console.log(time_B);
+			}, time_B);
+		}); 
 	};
 
-	let _setB = () => { 
-			_b = new Date().getTime() / 3
-			return _b;
+	_setC = (val) => {
+		
+		return new Promise(resolve => {
+		   const time_C = Math.floor(Math.random() * (300 - 10 + 1) + 10);
+		    setTimeout(() => {
+  		        this._c = val * 10;;
+			// console.log (this._c);
+			resolve (time_C);
+			// console.log(time_C);
+			}, time_C);
+		}); 
 	};
 
-	let setTimeout_B = () => { 
-		let time_B = Math.floor(Math.random() * (220 - 30 + 1) + 30);
-		setTimeout (_setB(), time_B);
-		return time_B;
-	};
-
-	let _setC = (val) => { 
-		_c = val * 10;
-		return _c;
-	};
-
-	let setTimeout_C = (val) => { 
-		let time_C = Math.floor(Math.random() * (300 - 10 + 1) + 10);
-		setTimeout (_setC(val), time_C);
-		return time_C;
-	};
 
 	this.process = () => {
 
-		return Promise.all([setTimeout_A(), setTimeout_B()]).
-		then (result => {
-			let tmp = (setTimeout_A() + setTimeout_B())/2;
-			// console.log (tmp);
-			return tmp;
-			}).
-		then (tmp => {
-			let avg = (tmp+setTimeout_C(tmp))/3;
-			return avg; 
-			});
+		return Promise.all([_setA(), _setB()]).
+		
+		then(([time_A, time_B]) => {
+                	let tmp = (time_A + time_B)/2;
+                	time1 = time_A;
+                	time2 = time_B;
+                	return _setC(tmp);
+            	}).
+		
+		then(tmp => {
+                	time3 = tmp;
+            	})
 
-		};
+            	.then((res) => { 
+            		res = (time1 + time2 + time3)/3;
+            		// console.log (res);
+            		return res;
+            	});
+	};
 
 	this.show = () => {
-		console.log (`a ${_setA()}`);
-		console.log (`b ${_setB()}`);
-		console.log (`c ${_setC((setTimeout_A() + setTimeout_B())/2)}`);
+		console.log (`a ${this._a}`);
+		console.log (`b ${this._b}`);
+		console.log (`c ${this._c}`);
 		};
 
 
 };
 
 a = new A;
-console.log(a.process());
-a.show();
+// console.log (a.process());
+// a.show();
 
-// const showData = () => {
-//   let i = 1;
-//   let timerId = setInterval(function() {
-//     console.log(a.show());
-//     if (i == 30) clearInterval(timerId);
-//     i++;
-//   }, 500);
-// }
-
-// showData();
-
-for (let j=1; j<10001; j++) {
-	let tmp = a.process(); console.log(tmp);
-	if (tmp < 100) {
-		console.log(`Break_i${j}`);
-		console.log(`Is this number${tmp}`);
-		break;
-	} else 
-	if (j==10000) {
-			console.log('Operation full done');
-	};	
+const showData = setInterval(() => 
+     a.show(), 500);
 
 
+const showProcess = (i) => {
+
+	let j=i;
+
+	a.process().then(resProcess => {
+
+		    console.log(`Number of iteration_i ${j}, this number ${resProcess}`);
+		    j++;
+
+     	   	if (resProcess<100) {
+
+     	        clearInterval(showData);
+                console.log(`Break_i ${j}`);
+				console.log(`Is this number ${resProcess}`);
+
+      		   } else {
+
+      		if (j < 10001) {
+      			showProcess(j);
+      		} else {
+
+      		   	clearInterval(showData);
+      		   	console.log('Operation full done');
+
+      		   }
+      		}
+     });
 };
+
+	showProcess(i=0);
+
+
+
+
+
