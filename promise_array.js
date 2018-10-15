56088d2bf469dfc3d8238c6d574623fcdc54a115
slavailchenko35 <slavailchenko35@gmail.com>
2018-10-15 12:14:23 +0300

    v1.48

---
+++ promise_array.js
@@ -0,0 +1,97 @@
+function MyApp (array) {
+
+  this.array = array;
+
+  this.mapParallel = (callback) => {
+    return Promise.all(this.array.map(callback)).then(data => 
+    console.log(data)
+	)};	
+
+  this.filterParallel = (callback) => {
+    return Promise.all(this.array.filter(callback)).then(data => 
+    console.log(data)
+    )}; 
+
+  this.reducePromise = (fn) => {
+    return this.array.reduce(
+        (a, i) => a.then((arr) => fn(i).then(d => arr.concat([d]))),
+        Promise.resolve([])
+    )
+}   
+
+}
+
+const dealerships = [
+{
+        "dealershipId" : 113,
+        "name" : "Drezdner Chevrolet",
+        "state" : "NY",
+        "sellingArea" : [ 
+            "NY", 
+            "NJ", 
+            "CT"
+        ]
+    },
+{
+        "dealershipId" : 100002,
+        "name" : "VIP Westbury CDJR",
+        "state" : "NY",
+        "sellingArea" : [ 
+            "NY", 
+            "NJ", 
+            "CT"
+        ]
+    },
+{
+        "dealershipId" : 100145,
+        "name" : "Honda of Thousand Oaks",
+        "state" : "CA",
+        "sellingArea" : [ 
+            "CA"
+        ]
+    },
+{
+        "dealershipId" : 100138,
+        "name" : "Route 44 Hyundai",
+        "state" : "MA",
+        "sellingArea" : [ 
+            "MA", 
+            "RI"
+        ]
+    },
+    {
+        "dealershipId" : 100146,
+        "name" : "Crown CDJR",
+        "state" : "CA",
+        "sellingArea" : [ 
+            "CA"
+        ]
+    }
+];
+
+const a = new MyApp (dealerships);
+
+let arrayMap = a.mapParallel ((item) => {
+	return new Promise(resolve => setTimeout(() => {
+		if (item.state == 'CA') item.isActive = true;
+		resolve(item);
+	}, 0));
+});
+
+console.log (arrayMap);
+
+let arrayFilter = a.filterParallel ((item) => {
+    return new Promise(resolve => setTimeout(() => {
+        if (item.state === 'CA') resolve(item);
+    }, 200));
+});
+
+console.log (arrayFilter);
+
+let fn = (i) => new Promise(res => {
+    if (i.state == 'CA') i.isActive = true; res(i)})
+
+let arrayReduce = a.reducePromise(fn).
+            then(data => console.log(data));
+
+console.log (arrayReduce);
